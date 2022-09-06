9e1967a27c2c26ad1ccbae552ca2ef016739a3bc
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-09-06 07:32:40 +0100

    testing using aaa

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function sum (a:number, b:number):number{
+    return a + b
+  }
+
+  describe ("Sum" , () => {
+    describe("Given two number",  () => {
+      describe("When both numbers are postive integers", () => {
+        //arrange 
+        const firstNumber = 5;
+        const secondNumber = 10;
+        it("Then it returns the correct addition of both", () => {
+          //ACT
+          const result =sum(firstNumber, secondNumber);
+          //ASSERT
+          expect(result).toEqual(15);
+        })
+      })
+    })
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
